d44f812fcfafb925968c3128f8bdf9141c34194e
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-06-25 11:13:54 +0300

    Completed Day 2

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -34,16 +34,32 @@ class ViewController: UIViewController {
 
 		let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
 
+		var previous: UILabel?
+
 		viewsDictionary.values.forEach {
 			$0.translatesAutoresizingMaskIntoConstraints = false
 			$0.sizeToFit()
 			view.addSubview($0)
-		}
 
+			// Constraints
+			$0.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+			$0.heightAnchor.constraint(equalToConstant: 64).isActive = true
+			if let previous = previous {
+				$0.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+			} else {
+				$0.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+			}
+			previous = $0
+		}
+	}
+
+	private func createVFLConstraints(viewsDictionary: [String: UIView]) {
+		// An example of Visual Formatting Language constraints
 		viewsDictionary.keys.forEach {
 			view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\($0)]|", options: [], metrics: nil, views: viewsDictionary))
 		}
-		view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
+
+		let metrics = ["labelHeight": 88]
+		view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|", options: [], metrics: metrics, views: viewsDictionary))
 	}
 }
-
